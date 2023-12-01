74b865b611ee357c1dd47199031c80909737f663
Milan Mastilović <79919009+MilanMasta@users.noreply.github.com>
2023-12-01 18:55:22 +0100

    instaling shadcn

---
+++ components/Logo.tsx
@@ -0,0 +1,27 @@
+
+import LogoImage from '@logos/black.svg'
+import Link from 'next/link'
+import Image from 'next/image'
+import { AspectRatio } from './ui/aspect-ratio'
+
+const Logo = () => {
+  return (
+    <Link href="/" prefetch={false} className='pverflow-hidden'>
+      <div className="flex items-center w-72 h-14">
+        <AspectRatio
+          ratio={16 / 9}
+          className='flex item-center justify-center'
+        >
+          <Image
+            priority
+            src={LogoImage}
+            alt='Logo'
+            className="dark:filter dark:invert"
+          />
+        </AspectRatio>
+      </div>
+    </Link>
+  )
+}
+
+export default Logo
