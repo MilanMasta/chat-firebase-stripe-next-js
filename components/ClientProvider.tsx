72ef4968f7cb620576ee305b125a26a5b9a2bb93
Milan Mastilović <79919009+MilanMasta@users.noreply.github.com>
2023-12-15 16:41:14 +0100

    Add Google Analytics to ClientProvider component

--- components/ClientProvider.tsx
+++ components/ClientProvider.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import { SessionProvider } from "next-auth/react"
-
+import GoogleAnalytics from "@bradgarropy/next-google-analytics"
 export default function ClientProvider({
     children
-} : {
+}: {
     children: React.ReactNode;
-}){
-    return <SessionProvider>{children}</SessionProvider>
+}) {
+    return <SessionProvider>{children}
+        <>
+            <GoogleAnalytics measurementId="G-Q1NV212YX8" />
+        </>
+    </SessionProvider>
 }
